7c7f76fcafb5609c81e29f6cafb6466d5f3a932f
aish0526 <jadhavgaishwarya2003.com>
2025-07-04 22:52:16 +0530

    updated

--- src/Components/homepage.jsx
+++ src/Components/homepage.jsx
@@ -12,7 +12,7 @@ export default function HomePage() {
   const photos = ['/image1.jpg','/image2.jpg','/image3.jpg',
                   '/image4.jpg','/image5.jpg','/image6.jpg','/image7.jpg','/image8.jpg','/image9.jpg','/image10.jpg','/image11.jpg','/image12.jpg','/image13.jpg','/image14.jpg','/image15.jpg'];
                   
-  const masonryCols = { default: 3, 1100: 2, 700: 1 };
+  const masonryCols = { default: 4, 1100: 2, 400: 2 };
 
   return (
     <>
